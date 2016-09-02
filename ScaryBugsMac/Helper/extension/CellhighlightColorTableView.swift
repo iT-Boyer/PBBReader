94adc0d0a80310c43c03b1fb40d876e129db7ba4
huoshuguang <huoshuguang@0c13ac06-f049-7c4f-b8ca-8e446896e38f>
2016-09-02 01:54:41 +0000

    版本1.0.0.4
    git-svn-id: https://192.168.85.64/svn/PBBforIOS/PBBReaderForOSX@766 0c13ac06-f049-7c4f-b8ca-8e446896e38f

---
+++ ScaryBugsMac/Helper/extension/CellhighlightColorTableView.swift
@@ -0,0 +1,21 @@
+//
+//  CellhighlightColorTableView.swift
+//  ScaryBugsMac
+//
+//  Created by pengyucheng on 16/9/1.
+//  Copyright © 2016年 recomend. All rights reserved.
+//
+
+import Cocoa
+
+class CellhighlightColorTableView: NSTableView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+    
+    
+}
