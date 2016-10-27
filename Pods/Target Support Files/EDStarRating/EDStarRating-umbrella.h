1a17b291a31464b3ed774666918d8166673b6e95
huoshuguang <huoshuguang@8d974d90-fa6c-b240-8058-8662f63bf965>
2016-10-27 07:15:00 +0000

    得到的
    git-svn-id: https://192.168.85.6/svn/PBBReader_Mac/trunk@21 8d974d90-fa6c-b240-8058-8662f63bf965

---
+++ Pods/Target Support Files/EDStarRating/EDStarRating-umbrella.h
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+#import "EDStarRating.h"
+
+FOUNDATION_EXPORT double EDStarRatingVersionNumber;
+FOUNDATION_EXPORT const unsigned char EDStarRatingVersionString[];
+
---
+++ Pods/Target Support Files/Pods-PBBReader/Pods-PBBReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PBBReader : NSObject
+@end
+@implementation PodsDummy_Pods_PBBReader
+@end
