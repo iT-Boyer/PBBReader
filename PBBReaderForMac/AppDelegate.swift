701e48aac61cf13e29e3b7970c83a8619532c1e6
huos3203 <724987481@qq.com>
2016-10-30 18:36:48 +0800

    添加Fabric支持osx,仍在测试中

--- PBBReaderForMac/AppDelegate.swift
+++ PBBReaderForMac/AppDelegate.swift
@@ -7,10 +7,13 @@
 //
 
 import Cocoa
+//import Fabric
+//import Crashlytics
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate{
 
+//    Fabric.with([Crashlytics.self])
     //必须声明为全局属性，否则在声明PycFile调用delegate时，delegate = nil
     //还出现第一次启动执行两次openFiles方法
     let appHelper = AppDelegateHelper()
