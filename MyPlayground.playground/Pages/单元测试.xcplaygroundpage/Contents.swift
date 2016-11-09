077aacab0b9f81cd882a812e51873900e3b83763
huoshuguang <huoshuguang@8d974d90-fa6c-b240-8058-8662f63bf965>
2016-11-09 06:30:36 +0000

    单元测试
    git-svn-id: https://192.168.85.6/svn/PBBReader_Mac/trunk@58 8d974d90-fa6c-b240-8058-8662f63bf965

---
+++ MyPlayground.playground/Pages/单元测试.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: 检测两个字母串是否是仅排序不同的相同字母组成的词。
+func checkWord(_ word:String,_ isAnagramOfWord:String)->Bool
+{
+        return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: [Next](@next)
