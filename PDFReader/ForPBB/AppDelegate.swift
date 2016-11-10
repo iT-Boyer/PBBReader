9ee8acd49a1efedc5f0c53a13a75ef18cec69edc
huoshuguang <huoshuguang@8d974d90-fa6c-b240-8058-8662f63bf965>
2016-11-10 07:40:54 +0000

    跑通PBBReader 开始着手ForPBBMac延伸开发
    git-svn-id: https://192.168.85.6/svn/PBBReader_Mac/trunk@63 8d974d90-fa6c-b240-8058-8662f63bf965

---
+++ PDFReader/ForPBB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ForPBB
+//
+//  Created by pengyucheng on 10/11/2016.
+//  Copyright © 2016 PBBReader. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PDFReader/ForPBB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ForPBB
+//
+//  Created by pengyucheng on 10/11/2016.
+//  Copyright © 2016 PBBReader. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
