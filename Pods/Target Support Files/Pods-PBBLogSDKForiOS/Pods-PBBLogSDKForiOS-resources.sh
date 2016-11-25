35cc28763923b90097600cad96161316be3e78d1
huoshuguang <huoshuguang@8d974d90-fa6c-b240-8058-8662f63bf965>
2016-11-25 01:38:44 +0000

    install pod
    git-svn-id: https://192.168.85.6/svn/PBBReader_Mac/trunk@106 8d974d90-fa6c-b240-8058-8662f63bf965

--- Pods/Target Support Files/Pods-PBBLogSDKForiOS/Pods-PBBLogSDKForiOS-resources.sh
+++ Pods/Target Support Files/Pods-PBBLogSDKForiOS/Pods-PBBLogSDKForiOS-resources.sh
@@ -18,26 +18,17 @@ case "${TARGETED_DEVICE_FAMILY}" in
   2)
     TARGET_DEVICE_ARGS="--target-device ipad"
     ;;
-<<<<<<< HEAD
-=======
-  3)
-    TARGET_DEVICE_ARGS="--target-device tv"
-    ;;
->>>>>>> a826ee56b0e3bbad46316df397308dfaafd5646f
   *)
     TARGET_DEVICE_ARGS="--target-device mac"
     ;;
 esac
 
-<<<<<<< HEAD
 realpath() {
   DIRECTORY="$(cd "${1%/*}" && pwd)"
   FILENAME="${1##*/}"
   echo "$DIRECTORY/$FILENAME"
 }
 
-=======
->>>>>>> a826ee56b0e3bbad46316df397308dfaafd5646f
 install_resource()
 {
   if [[ "$1" = /* ]] ; then
@@ -57,13 +48,8 @@ EOM
       ibtool --reference-external-strings-file --errors --warnings --notices --minimum-deployment-target ${!DEPLOYMENT_TARGET_SETTING_NAME} --output-format human-readable-text --compile "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$RESOURCE_PATH\" .storyboard`.storyboardc" "$RESOURCE_PATH" --sdk "${SDKROOT}" ${TARGET_DEVICE_ARGS}
       ;;
     *.xib)
-<<<<<<< HEAD
       echo "ibtool --reference-external-strings-file --errors --warnings --notices --minimum-deployment-target ${!DEPLOYMENT_TARGET_SETTING_NAME} --output-format human-readable-text --compile ${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$RESOURCE_PATH\" .xib`.nib $RESOURCE_PATH --sdk ${SDKROOT}"
       ibtool --reference-external-strings-file --errors --warnings --notices --minimum-deployment-target ${!DEPLOYMENT_TARGET_SETTING_NAME} --output-format human-readable-text --compile "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$RESOURCE_PATH\" .xib`.nib" "$RESOURCE_PATH" --sdk "${SDKROOT}"
-=======
-      echo "ibtool --reference-external-strings-file --errors --warnings --notices --minimum-deployment-target ${!DEPLOYMENT_TARGET_SETTING_NAME} --output-format human-readable-text --compile ${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$RESOURCE_PATH\" .xib`.nib $RESOURCE_PATH --sdk ${SDKROOT} ${TARGET_DEVICE_ARGS}"
-      ibtool --reference-external-strings-file --errors --warnings --notices --minimum-deployment-target ${!DEPLOYMENT_TARGET_SETTING_NAME} --output-format human-readable-text --compile "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$RESOURCE_PATH\" .xib`.nib" "$RESOURCE_PATH" --sdk "${SDKROOT}" ${TARGET_DEVICE_ARGS}
->>>>>>> a826ee56b0e3bbad46316df397308dfaafd5646f
       ;;
     *.framework)
       echo "mkdir -p ${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
@@ -84,11 +70,7 @@ EOM
       xcrun mapc "$RESOURCE_PATH" "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$RESOURCE_PATH" .xcmappingmodel`.cdm"
       ;;
     *.xcassets)
-<<<<<<< HEAD
       ABSOLUTE_XCASSET_FILE=$(realpath "$RESOURCE_PATH")
-=======
-      ABSOLUTE_XCASSET_FILE="$RESOURCE_PATH"
->>>>>>> a826ee56b0e3bbad46316df397308dfaafd5646f
       XCASSET_FILES+=("$ABSOLUTE_XCASSET_FILE")
       ;;
     *)
@@ -111,11 +93,7 @@ then
   # Find all other xcassets (this unfortunately includes those of path pods and other targets).
   OTHER_XCASSETS=$(find "$PWD" -iname "*.xcassets" -type d)
   while read line; do
-<<<<<<< HEAD
     if [[ $line != "`realpath $PODS_ROOT`*" ]]; then
-=======
-    if [[ $line != "${PODS_ROOT}*" ]]; then
->>>>>>> a826ee56b0e3bbad46316df397308dfaafd5646f
       XCASSET_FILES+=("$line")
     fi
   done <<<"$OTHER_XCASSETS"
--- Pods/Target Support Files/RNCryptor-iOS/RNCryptor-iOS-umbrella.h
+++ Pods/Target Support Files/RNCryptor-iOS/RNCryptor-iOS-umbrella.h
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 #import <UIKit/UIKit.h>
-=======
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
-#endif
->>>>>>> a826ee56b0e3bbad46316df397308dfaafd5646f
 
 #import "RNCryptor.h"
 
