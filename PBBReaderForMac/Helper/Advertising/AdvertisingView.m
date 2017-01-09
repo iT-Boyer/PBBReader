438d3d19dae962ea2cc0fd9b6920e5e8b977f005
huoshuguang <huoshuguang@8d974d90-fa6c-b240-8058-8662f63bf965>
2017-01-09 08:02:38 +0000

    V29版本
    git-svn-id: https://192.168.85.6/svn/PBBReader_Mac/trunk@173 8d974d90-fa6c-b240-8058-8662f63bf965

--- PBBReaderForMac/Helper/Advertising/AdvertisingView.m
+++ PBBReaderForMac/Helper/Advertising/AdvertisingView.m
@@ -13,43 +13,6 @@
 #import "MBProgressHUD.h"
 #define THERMOMETER_FRAME (20, 5, 25, 5);
 @implementation AdvertisingView
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-//UIView构造方法
-- (id)initWithFrame:(CGRect)aRect
-{
-    if ((self = [super initWithFrame:aRect])) {
-        [self commonInit];
-    }
-    return self;
-}
-
-//Storyboard用
-- (id)initWithCoder:(NSCoder*)coder
-{
-    if ((self = [super initWithCoder:coder])) {
-        [self commonInit];
-    }
-    return self;
-}
-
-- (void)commonInit
-{
-    /* 这里开始初始化 */
-    
-    //如果需要重新调用drawRect则设置contentMode为UIViewContentModeRedraw
-//    self.contentMode = NSViewContentModeRedraw;
-    //不允许从Autoresizing转换Autolayout的Constraints
-    //貌似Storyboard创建时调用initWithCoder方法时translatesAutoresizingMaskIntoConstraints已经是NO了
-    self.translatesAutoresizingMaskIntoConstraints = NO;
-
-}
-
 
 //设置Autolayout中的边距辅助方法
 - (void)setEdge:(NSView*)superview view:(NSView*)view attr:(NSLayoutAttribute)attr constant:(CGFloat)constant
@@ -67,8 +30,8 @@
                 isOutLine:(BOOL)OutLine
 {
     //重置缓存图片
-//    [_ibImageView setImage:nil];
-    [_ibImageView setImage:[NSImage imageNamed:@"advitising.jpg"]];
+    [_ibImageView setImage:nil];
+//    [_ibImageView setImage:[NSImage imageNamed:@"advitising.jpg"]];
     if(![keywindow.contentView isKindOfClass:[NSView class]])
     {
         _finish = YES;
@@ -76,7 +39,8 @@
     }
     
     NSView *keyView = keywindow.contentView;
-//    keyView.translatesAutoresizingMaskIntoConstraints = false;
+    self.wantsLayer = true;
+    self.translatesAutoresizingMaskIntoConstraints = NO;
     [keyView addSubview:self];
     [self setEdge:keyView view:self attr:NSLayoutAttributeTop constant:0];
     [self setEdge:keyView view:self attr:NSLayoutAttributeBottom constant:0];
